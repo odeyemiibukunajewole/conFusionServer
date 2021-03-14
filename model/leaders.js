ed65cc086e12ab6efae8eb9eaf82d44c2068320e
Odeyemi Ibukun <odeyemiibukuna@gmail.com>
2021-03-14 06:50:47 +0100

    add cookies

---
+++ model/leaders.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+require('mongoose-currency').loadType(mongoose)
+const Currency = mongoose.Types.Currency
+
+const leadersSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default: false
+    },
+    abbr: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leadersSchema);
+
+module.exports = Leaders;
